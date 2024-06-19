864f1a5fab433ae8637377567644a24d2bb6f55b
Vincent Leclerc <vleclerc@inow.fr>
2024-06-19 10:00:27 -0400

    with var

--- karucar-api/config.py
+++ karucar-api/config.py
@@ -1,8 +1,12 @@
 import os
 
 class Config:
+    #SQLALCHEMY_DATABASE_URI = (
+    #    f'mysql+pymysql://user1:user1@/'
+    #    f'karu-db?unix_socket=/cloudsql/vincent-exelcia:europe-west9:formation-db'
+    #)
     SQLALCHEMY_DATABASE_URI = (
-        f'mysql+pymysql://user1:user1@/'
-        f'karu-db?unix_socket=/cloudsql/vincent-exelcia:europe-west9:formation-db'
+        f'mysql+pymysql://{os.getenv("DB_USER")}:{os.getenv("DB_PASS")}@/'
+        f'{os.getenv("DB_NAME")}?unix_socket=/cloudsql/{os.getenv("CLOUD_SQL_CONNECTION_NAME")}'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
